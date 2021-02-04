a5a49a70bff1937e8417f06c59b641f7f11b7611
anosaryev <anthonynos@outlook.com>
2021-02-04 15:29:54 -0500

    made reverse

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,8 +1,14 @@
 public class Recursion{
 
-  //public static String reverse(String s){}
+  public static String reverse(String s) {
+	if (s.length() < 2){
+		return s;
+	}
+	return reverse(s.substring(1)) + s.charAt(0);
+}
 
   //public static long countNoDoubleLetterWords(int length, String word){}
 
   //public static double sqrt(double n){}
+
 }

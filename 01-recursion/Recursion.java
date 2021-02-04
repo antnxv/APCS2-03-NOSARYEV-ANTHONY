f579518c9cdf5d1feb829721631d1b9587324a1b
anosaryev <anthonynos@outlook.com>
2021-02-04 16:04:35 -0500

    made countNoDoubleLetterWords

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,14 +1,30 @@
 public class Recursion{
 
-  public static String reverse(String s) {
+  public static String reverse(String s){
     if (s.length() < 2){
       return s;
     }
     return reverse(s.substring(1)) + s.charAt(0);
   }
 
-  //public static long countNoDoubleLetterWords(int length, String word){}
+  public static long countNoDoubleLetterWords(int length, String word){
+    if (length == 0){
+      return 1;
+    }else{
+      long count = 0;
+      for (char i = 'a'; i <= 'z'; i ++){
+        if (word.length() == 0 || word.charAt(word.length()-1) != i){
+          count += countNoDoubleLetterWords(length-1, word + i);
+        }
+      }
+      return count;
+    }
+
+  }
 
   //public static double sqrt(double n){}
 
+  public static void main(String[] args) {
+    System.out.println(countNoDoubleLetterWords(7, ""));
+  }
 }

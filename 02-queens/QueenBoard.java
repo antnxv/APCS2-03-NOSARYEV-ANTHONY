b2ad34e62c1a3bdb16c93a7f665c9a431a84c435
anosaryev <anthonynos@outlook.com>
2021-02-10 16:15:21 -0500

    made addQueen(), fixed up toString()

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -4,14 +4,25 @@ public class QueenBoard{
 
   public QueenBoard(int size){
     board = new int[size][size];
-    for (int[] i: board){
-      for (int j = 0; j < size; j ++){
-        i[j] = 0;
+    for (int[] y: board){
+      for (int x = 0; x < size; x ++){
+        y[x] = 0;
       }
     }
   }
 
-  //private boolean addQueen(int r, int c){}
+  public boolean addQueen(int r, int c){
+    if (board[c][r] != 0) return false;
+    for (int y = 0; y < board.length; y ++){
+      for (int x = r; x < board.length; x ++){
+        if (x == r || y == c || Math.abs(x-r) == Math.abs(y-c)){
+          board[y][x] ++;
+        }
+      }
+    }
+    board[c][r] = -1;
+    return true;
+  }
 
   //private void removeQueen(int r, int c){}
 
@@ -23,8 +34,9 @@ public class QueenBoard{
     String str = "";
     for (int[] i: board){
       for (int j: i){
-        if (j == 0) str += "_ ";
-        else str += "Q ";
+        if (j == -1) str += "Q ";
+        else if (j > 0) str += j + " ";
+        else str += "_ ";
       }
       str += "\n";
     }

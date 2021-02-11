4e5e91f87eaa76a7a93cd95a24dddda89532bacf
anosaryev <anthonynos@outlook.com>
2021-02-10 20:17:34 -0500

    made solve(), fixed up some other methods

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -2,40 +2,66 @@ public class QueenBoard{
 
   private int[][] board;
 
-  public QueenBoard(int size){
-    board = new int[size][size];
+  public QueenBoard(int length){
+    board = new int[length][length];
     for (int[] y: board){
-      for (int x = 0; x < size; x ++){
-        y[x] = 0;
+      for (int x: y){
+        x = 0;
       }
     }
   }
 
-  public boolean addQueen(int r, int c){
-    if (board[c][r] != 0) return false;
+  private boolean addQueen(int r, int c){
+    if (board[r][c] != 0) return false;
     for (int y = 0; y < board.length; y ++){
-      for (int x = r; x < board.length; x ++){
-        if (x == r || y == c || Math.abs(x-r) == Math.abs(y-c)){
+      for (int x = c; x < board.length; x ++){
+        if (x == c || y == r || Math.abs(x-c) == Math.abs(y-r)){
           board[y][x] ++;
         }
       }
     }
-    board[c][r] = -1;
+    board[r][c] = -1;
     return true;
   }
 
-  public void removeQueen(int r, int c){
+  private void removeQueen(int r, int c){
     for (int y = 0; y < board.length; y ++){
-      for (int x = r; x < board.length; x ++){
-        if (x == r || y == c || Math.abs(x-r) == Math.abs(y-c)){
+      for (int x = c; x < board.length; x ++){
+        if (x == c || y == r || Math.abs(x-c) == Math.abs(y-r)){
           board[y][x] --;
         }
       }
     }
-    board[c][r] = 0;
+    board[r][c] = 0;
   }
 
-  //public boolean solve(){}
+  public boolean solve(){
+    return solve(0, 0);
+  }
+
+  public boolean solve(int r, int c){
+    if (board[0][0] != 0) throw new IllegalStateException("board must be empty");
+    if (c == board.length) return true;
+    if (r == board.length){
+      if (c == 0){
+        for (int[] y: board){
+          for (int x: y){
+            x = 0;
+          }
+        }
+      }
+      return false;
+    }
+    if (addQueen(r, c)){
+      if (solve(0, c+1)){
+        return true;
+      }else{
+        removeQueen(r, c);
+        return solve(r+1, c);
+      }
+    }
+    return solve(r+1, c);
+  }
 
   //public int countSolutions(){}
 
@@ -44,7 +70,7 @@ public class QueenBoard{
     for (int[] i: board){
       for (int j: i){
         if (j == -1) str += "Q ";
-        else if (j > 0) str += j + " ";
+        //else if (j > 0) str += j + " ";
         else str += "_ ";
       }
       str += "\n";

33d012620f5da82b96657dbd8f1e7e6923e205be
anosaryev <anthonynos@outlook.com>
2021-03-04 20:35:08 -0500

    made silver

--- 04-USACO/USACO.java
+++ 04-USACO/USACO.java
@@ -45,8 +45,60 @@ public class USACO{
     return S*5184; // 36 sq ft in sq inches
   }
 
-  public long silver(String filename){ // NO RECURSION!
-    return 0;
+  public long silver(String filename) throws FileNotFoundException{
+    Scanner data = new Scanner(new File(filename));
+    int N = data.nextInt();
+    int M = data.nextInt();
+    int T = data.nextInt();
+    int[][] map = new int[N][M];
+    for (int y = 0; y < N; y ++){
+      String line = data.next();
+      for (int x = 0; x < M; x ++){
+        if (line.charAt(x) == '.'){
+          map[y][x] = 0;
+        }else{
+          map[y][x] = -1;
+        }
+      }
+    }
+    int R1 = data.nextInt()-1;
+    int C1 = data.nextInt()-1;
+    int R2 = data.nextInt()-1;
+    int C2 = data.nextInt()-1;
+    map[R1][C1] = 1;
+    while (T > 0){
+      T --;
+      int[][] nap = new int[N][M];
+      for (int y = 0; y < N; y ++){
+        for (int x = 0; x < M; x ++){
+          if (map[y][x] == -1){
+            nap[y][x] = -1;
+            continue;
+          }
+          if (map[y][x] > 0){
+            nap[y][x] = 0;
+          }
+          int[][] combos = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+          for (int[] i: combos){
+            try{
+              int B = map[y+i[0]][x+i[1]];
+              if (B != -1)
+              nap[y][x] += B;
+            }catch (IndexOutOfBoundsException e){
+            }
+          }
+        }
+      }
+      map = nap;
+    }
+    return map[R2][C2];
+
+
+
+
+
+
+     // NO RECURSION!
     /**With more available steps, paths ripple:
 
     Mark trees as -1, don't add to updated paths for neighbours
@@ -63,13 +115,11 @@ public class USACO{
     0 1 0 1 0
     0 0 1 0 0
 
-    0 0 0 1 0 0 0
-    0 0 2 0 2 0 0
-    0 2 0 4 0 2 0
-    1 0 4 0 4 0 1
-    0 2 0 4 0 2 0
-    0 0 2 0 2 0 0
-    0 0 0 1 0 0 0
+    0 2 0 2 0
+    2 0 4 0 2
+    0 4 0 4 0
+    2 0 4 0 2
+    0 2 0 2 0
     **/
 
   }

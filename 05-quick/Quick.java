691f28ffbf0c0c52293ed97794033d030a0ece5c
anosaryev <anthonynos@outlook.com>
2021-03-11 00:30:23 -0500

    fixed pivot selection

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -4,15 +4,26 @@ public class Quick{
   }
 
   public static int partition(int[] data, int lo, int hi){
-    int j = (lo + hi)/2;
+    int mid = (lo + hi)/2;
+    int j;
+    if (data[mid] > data[lo] && data[mid] < data[hi]
+       || data[mid] < data[lo] && data[mid] > data[hi]){
+      j = mid;
+    }else if(data[lo] > data[mid] && data[lo] < data[hi]
+       || data[lo] < data[mid] && data[lo] > data[hi]){
+      j = lo;
+    }else{
+      j = hi;
+    }
+    System.out.println("data["+j+"] = "+data[j]);
     for (int i = lo; i <= hi; i ++){
-      if (data[i] > data[j] && i < j || data[i] == data[j] && Math.random()*2 == 0){
+      if (i < j && (data[i] > data[j] || (data[i] == data[j] && Math.random()*2 == 0))){
         int temp = data[i];
         data[i] = data[j];
         data[j] = temp;
         j = i;
       }
-      else if (data[i] < data[j] && i > j || data[i] == data[j]){
+      else if (i > j && (data[i] < data[j] || (data[i] == data[j] && Math.random()*2 == 0))){
         int temp = data[i];
         data[i] = data[j+1];
         data[j+1] = data[j];

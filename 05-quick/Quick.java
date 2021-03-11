f6bbd8d8079ec16b08d1320ff84aab2a30773081
anosaryev <anthonynos@outlook.com>
2021-03-10 23:46:25 -0500

    adapted partition from prelim

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -6,13 +6,13 @@ public class Quick{
   public static int partition(int[] data, int lo, int hi){
     int j = (lo + hi)/2;
     for (int i = lo; i <= hi; i ++){
-      if (data[i] >= data[j] && i < j){
+      if (data[i] > data[j] && i < j || data[i] == data[j] && Math.random()*2 == 0){
         int temp = data[i];
         data[i] = data[j];
         data[j] = temp;
         j = i;
       }
-      else if (data[i] < data[j] && i > j){
+      else if (data[i] < data[j] && i > j || data[i] == data[j]){
         int temp = data[i];
         data[i] = data[j+1];
         data[j+1] = data[j];

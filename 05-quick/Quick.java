1163e6914ad6b3b67852dd648f207e3cfc967b93
anosaryev <anthonynos@outlook.com>
2021-03-10 23:44:21 -0500

    adapted partition from prelim

---
+++ 05-quick/Quick.java
@@ -0,0 +1,25 @@
+public class Quick{
+  public static int quickselect(int[] data, int k){
+    return 0;
+  }
+
+  public static int partition(int[] data, int lo, int hi){
+    int j = (lo + hi)/2;
+    for (int i = lo; i <= hi; i ++){
+      if (data[i] >= data[j] && i < j){
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+        j = i;
+      }
+      else if (data[i] < data[j] && i > j){
+        int temp = data[i];
+        data[i] = data[j+1];
+        data[j+1] = data[j];
+        data[j] = temp;
+        j ++;
+      }
+    }
+    return j;
+  }
+}

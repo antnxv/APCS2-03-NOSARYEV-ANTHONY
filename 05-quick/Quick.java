86132cba866f87ed8e36707df4e150131c7ca4a8
anosaryev <anthonynos@outlook.com>
2021-03-11 17:05:15 -0500

    made dutch flag partition and dutch flag quicksort alternatives

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -11,6 +11,17 @@ public class Quick{
     }
   }
 
+  public static void quicksortDutch(int[] data){
+    quicksortDutch(data, 0, data.length-1);
+  }
+  public static void quicksortDutch(int[] data, int lo, int hi){
+    if (hi-lo > 1){
+      int[] j = partitionDutch(data, lo, hi);
+      quicksort(data, lo, j[0]-1);
+      quicksort(data, j[1]+1, hi);
+    }
+  }
+
   public static int quickselect(int[] data, int k){
     int j = partition(data, 0, data.length-1);
     if (j == k) return data[j];
@@ -30,17 +41,7 @@ public class Quick{
   }
 
   public static int partition(int[] data, int lo, int hi){
-    int mid = (lo + hi)/2;
-    int j;
-    if (data[mid] > data[lo] && data[mid] < data[hi]
-       || data[mid] < data[lo] && data[mid] > data[hi]){
-      j = mid;
-    }else if(data[lo] > data[mid] && data[lo] < data[hi]
-       || data[lo] < data[mid] && data[lo] > data[hi]){
-      j = lo;
-    }else{
-      j = hi;
-    }
+    int j = antmed(data, lo, (lo + hi)/2, hi);
     for (int i = lo; i <= hi; i ++){
       if (i < j && (data[i] > data[j] || (data[i] == data[j] && Math.random()*2 == 0))){
         int temp = data[i];
@@ -55,6 +56,49 @@ public class Quick{
         data[j] = temp;
         j ++;
       }
+    }
+    return j;
+  }
+
+  public static int[] partitionDutch(int[] data, int lo, int hi){
+    int lt = antmed(data, lo, (lo + hi)/2, hi);
+    int gt = lt;
+    for (int i = lo; i <= hi; i ++){
+      if (i < lt && data[i] >= data[lt]){
+        int temp = data[lt-1];
+        data[lt-1] = data[gt];
+        data[gt] = data[i];
+        data[i] = temp;
+        lt --;
+        if (data[gt] != data[lt]){
+          gt --;
+        }
+      }
+      else if (i > gt && data[i] <= data[gt]){
+        int temp = data[i];
+        data[i] = data[gt+1];
+        data[gt+1] = data[lt];
+        data[lt] = temp;
+        gt ++;
+        if (data[lt] != data[gt]){
+          lt ++;
+        }
+      }
+    }
+    int[] sub = {lt, gt};
+    return sub;
+  }
+
+  public static int antmed(int[] data, int lo, int mid, int hi){
+    int j;
+    if (data[mid] > data[lo] && data[mid] < data[hi]
+       || data[mid] < data[lo] && data[mid] > data[hi]){
+      j = mid;
+    }else if(data[lo] > data[mid] && data[lo] < data[hi]
+       || data[lo] < data[mid] && data[lo] > data[hi]){
+      j = lo;
+    }else{
+      j = hi;
     }
     return j;
   }

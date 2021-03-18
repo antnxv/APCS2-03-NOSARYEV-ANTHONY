a20842188dbb046b969213b5ec9b531c35b18fc4
anosaryev <anthonynos@outlook.com>
2021-03-18 16:01:40 -0400

    made constructors

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -3,11 +3,21 @@ public class MyDeque<E>{
   private int size, start, end;
 
   public MyDeque(){
-
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
+    start = 9;
+    end = 8;
+    size = 0;
   }
 
   public MyDeque(int initialCapacity){
-
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+    start = initialCapacity-1;
+    end = initialCapacity-2;
+    size = 0;
   }
 
   public int size(){

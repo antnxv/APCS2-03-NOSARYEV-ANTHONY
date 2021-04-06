de67017877618e7078014f1a04f97fa0bf2980f4
anosaryev <anthonynos@outlook.com>
2021-04-06 01:27:54 -0400

    I completely restarted this assignment because my old MyDeque() was just not working. This time around, I reworked the constructors, toString and resize to be less dependent on my old idea of having first elements on the right and last elements on the left at all time because this strict concept I set for myslef lead to a lot of bad coding and specific edge-case handling rather than general coding. Essentially I've placed more emphasis on start and end and that means the only real edge case I've had to deal with is wrapping around the array (which is normal). I don't love using other people's testers because it feels really one-sided but nonetheless I tested with 3 different testers (4 including my occasional debugging).

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,21 +1,23 @@
+import java.util.*;
+
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
   public MyDeque(){
     @SuppressWarnings("unchecked")
-    E[] d = (E[])new Object[50];
+    E[] d = (E[]) new Object[51];
     data = d;
-    start = size = 0;
-    end = 49;
+    start = 1;
+    end = size = 0;
   }
 
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
-    E[] d = (E[])new Object[initialCapacity];
+    E[] d = (E[]) new Object[initialCapacity];
     data = d;
-    start = size = 0;
-    end = initialCapacity-1;
+    start = 1;
+    end = size = 0;
   }
 
   public int size(){
@@ -25,16 +27,26 @@ public class MyDeque<E>{
   public String toString(){
     if (size == 0) return "{}";
     String str = "{";
-    for (int i = start; i < data.length; i ++){
-      str += data[i]+", ";
-    }
-    for (int i = 0; i <= end; i ++){
-      str += data[i]+", ";
+    if (start <= end){
+      for (int i = start; i <= end; i ++){
+        str += data[i]+", ";
+      }
+    }else{
+      for (int i = start; i <= end+data.length; i ++){
+        if (i < data.length){
+          str += data[i]+", ";
+        }else{
+          str += data[i-data.length]+", ";
+        }
+      }
     }
     return str.substring(0, str.length()-2)+"}";
   }
 
   public void addFirst(E element){
+    if (element == null){
+      throw new NullPointerException("This deque does not permit null elements");
+    }
     expand();
     if (start == 0){
       start = data.length-1;
@@ -46,6 +58,9 @@ public class MyDeque<E>{
   }
 
   public void addLast(E element){
+    if (element == null){
+      throw new NullPointerException("This deque does not permit null elements");
+    }
     expand();
     if (end == data.length-1){
       end = 0;
@@ -59,20 +74,30 @@ public class MyDeque<E>{
   public void expand(){
     if (size == data.length){
       @SuppressWarnings("unchecked")
-      E[] d = (E[])new Object[1+data.length*2];
-      for (int i = 0; i <= end; i ++){
-        d[i] = data[i];
+      E[] d = (E[])new Object[data.length*2+1];
+      if (start <= end){
+        for (int i = start; i <= end; i ++){
+          d[i-start] = data[i];
+        }
+      }else{
+        for (int i = start; i <= end+data.length; i ++){
+          if (i < data.length){
+            d[i-start] = data[i];
+          }else{
+            d[i-start] = data[i-data.length];
+          }
+        }
       }
-      for (int i = start; i < data.length; i ++){
-        d[d.length-data.length+i] = data[i];
-      }
-      if (end == data.length-1) end = d.length-1;
-      if (start != 0) start = d.length-data.length+start;
+      start = 0;
+      end = size-1;
       data = d;
     }
   }
 
   public E removeFirst(){
+    if (size == 0){
+      throw new NoSuchElementException("This deque is empty");
+    }
     E temp = data[start];
     data[start] = null;
     if (start == data.length-1){
@@ -85,6 +110,9 @@ public class MyDeque<E>{
   }
 
   public E removeLast(){
+    if (size == 0){
+      throw new NoSuchElementException("This deque is empty");
+    }
     E temp = data[end];
     data[end] = null;
     if (end == 0){
@@ -97,10 +125,16 @@ public class MyDeque<E>{
   }
 
   public E getFirst(){
+    if (size == 0){
+      throw new NoSuchElementException("This deque is empty");
+    }
     return data[start];
   }
 
   public E getLast(){
+    if (size == 0){
+      throw new NoSuchElementException("This deque is empty");
+    }
     return data[end];
   }
 }

19bba4e6fdae71dbfcac795e4b13db1b50e51c25
anosaryev <anthonynos@outlook.com>
2021-04-22 23:08:26 -0400

    simplified pushDown syntactically, made buildHeap

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,25 +1,32 @@
 public class MyHeap{
   private static void pushDown(int[] data, int size, int index){
+    int ni;
     if (2*index+2 == size && data[2*index+1] > data[index]){
-      int temp = data[2*index+1];
-      data[2*index+1] = data[index];
-      data[index] = temp;
-      pushDown(data, size, 2*index+1);
-      return;
+      ni = 2*index+1;
     }
-    if (2*index+1 < size && data[2*index+1] > data[index] ||
-        2*index+2 < size && data[2*index+2] > data[index]){
-      int ni = (data[2*index+1] > data[2*index+2]) ? 2*index+1 : 2*index+2;
-      int temp = data[ni];
-      data[ni] = data[index];
-      data[index] = temp;
-      pushDown(data, size, ni);
+    else if (2*index+1 < size && data[2*index+1] > data[index]
+      || 2*index+2 < size && data[2*index+2] > data[index]){
+      ni = (data[2*index+1] > data[2*index+2]) ? 2*index+1 : 2*index+2;
     }
+    else return;
+    int temp = data[ni];
+    data[ni] = data[index];
+    data[index] = temp;
+    pushDown(data, size, ni);
   }
 
   public static void buildHeap(int data[]){
-    //filler
+    int index = data.length-1;
+    while (index > 0 && Math.log(index+2)/Math.log(2) % 1.0 != 0.0){
+      index --; // see bottom note
+    }
+    while (index >= 0){
+      pushDown(data, data.length, index);
+      index --;
+    }
   }
 }
 // children: 2n+1 (left) and 2n+2 (right)
 // parent: (n-1)//2 or just (n-1)/2 since ints always floor divide
+// n full levels makes 2^n -1 elements
+// node is right-most when index+2 is a power of 2
